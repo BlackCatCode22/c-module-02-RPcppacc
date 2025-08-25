da8fbd3b61f0965e36c7f835bf2888c5854c00bb
RPcppacc <repena559@gmail.com>
2025-08-25 11:01:56 -0700

    Add files via upload

---
+++ String Warmup.cpp
@@ -0,0 +1,27 @@
+// Made by Roman P 8/25/25
+//  String Warmup Code
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "\n\n Welcome to my String WarmUp Program \n\n";
+
+    // String Stuff
+
+    string mssge1 = " Hello this is a sting test";
+    //Output
+    cout << "\n the Message of the sting is: " << mssge1 << endl;
+
+    // use handy method for length
+
+    cout << "the legnth is of my message is: " << mssge1.length() << endl;
+    
+
+
+
+
+    return 0;
+}
